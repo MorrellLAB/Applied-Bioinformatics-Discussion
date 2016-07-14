46d3272893bd19557d924a110591e7239aabef71
mojaveazure <hoff0792@umn.edu>
2016-07-14 15:47:48 -0500

    Added stuff for R discussion

---
+++ Chapter8-R_Introduction/Coding_Challenges/Intermediate_Challenge.R
@@ -0,0 +1,26 @@
+#!/usr/bin/env Rscript
+
+############################################################
+##########          Start editing here            ##########
+############################################################
+#   Finish the function to plot Thetas values
+plotThetas <- function(window.center, thetas, label = 'Thetas', color = 'red'){
+    return(NULL)
+}
+
+############################################################
+##########          Leave this alone              ##########
+############################################################
+Thetas <- read.table(file = '', header = TRUE, as.is = TRUE)
+Thetas$Watterson <- Thetas$tW / Thetas$nSites
+Thetas$Pairwise <- Thetas$tP / Thetas$nSites
+
+############################################################
+##########          Start editing here            ##########
+############################################################
+#   Plot the values for Watterson's Theta,
+#   Pairwise Theta, and Tajima's D using
+#   the function you wrote
+plotThetas(label = "Watterson's Theta")
+plotThetas(label = "Pairwise Theta")
+plotThetas(label = "Tajima's D")
