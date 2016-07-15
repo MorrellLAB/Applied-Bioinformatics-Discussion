3f0e0dce1903e51f6adaa7480d5a2ca1556291be
mojaveazure <hoff0792@umn.edu>
2016-07-15 10:30:39 -0500

    Here you go, Chaochih

--- Chapter8-R_Introduction/Coding_Challenges/Intermediate_Challenge.R
+++ Chapter8-R_Introduction/Coding_Challenges/Intermediate_Challenge.R
@@ -1,6 +1,11 @@
 #!/usr/bin/env Rscript
 
 ############################################################
+##########          Leave this alone              ##########
+############################################################
+Thetas <- read.table(file = 'ANGSD-wrapper_sample.thetas', header = TRUE, as.is = TRUE)
+
+############################################################
 ##########          Start editing here            ##########
 ############################################################
 #   Finish the function to plot Thetas values
@@ -8,16 +13,9 @@ plotThetas <- function(window.center, thetas, label = 'Thetas', color = 'red'){
     return(NULL)
 }
 
-############################################################
-##########          Leave this alone              ##########
-############################################################
-Thetas <- read.table(file = 'ANGSD-wrapper_sample.thetas', header = TRUE, as.is = TRUE)
-Thetas$Watterson <- Thetas$tW / Thetas$nSites
-Thetas$Pairwise <- Thetas$tP / Thetas$nSites
+#   Add two new columns to the data frame
+#   to fix Watterson's and Pairwise Theta
 
-############################################################
-##########          Start editing here            ##########
-############################################################
 #   Plot the values for Watterson's Theta,
 #   Pairwise Theta, and Tajima's D using
 #   the function you wrote
